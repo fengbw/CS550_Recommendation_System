32cbcd43c3b3a607d28c831b49ff73086a9c5e12
fengbw <fengbwseu@163.com>
2020-04-20 11:48:36 -0400

    add data split

--- rs.py
+++ rs.py
@@ -1,4 +1,5 @@
 import json
+import collections
 
 def readData():
     data = []
@@ -7,8 +8,25 @@ def readData():
             record = json.loads(line)
             record = {key: record[key] for key in record.keys() & ('reviewerID', 'asin', 'overall')}
             data.append(record)
-    print(data[0])
     return data
+
+def splitData(data):
+    usersCount = collections.defaultdict(int)
+    for record in data:
+        usersCount[record['reviewerID']] += 1
+    for key in usersCount:
+        usersCount[key] //= 1.25
+    trainData = []
+    testData = []
+    for record in data:
+        if usersCount[record['reviewerID']] > 0:
+            trainData.append(record)
+            usersCount[record['reviewerID']] -= 1
+        else:
+            testData.append(record)
+    return trainData, testData
 
 if __name__ == '__main__':
     data = readData()
+    trainData, testData = splitData(data)
+    print(len(trainData), len(testData))
