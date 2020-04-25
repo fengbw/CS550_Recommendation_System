355f2f1128e7d128b0d7a96d73224c364b0fe1c7
congdeng <1829929@qq.com>
2020-04-24 22:42:01 -0400

    Data reveal.

--- DataReveal.py
+++ DataReveal.py
@@ -1,19 +1,21 @@
 import json
 import matplotlib.pyplot as plt
+import collections
 
 def readData():
-    data = []
+    data = [[] for i in range(3)]
     with open('Digital_Music_5.json', 'r') as f:
         for line in f:
             record = json.loads(line)
-            record = record['overall']
-            data.append(record)
+            data[0].append(record['reviewerID'])
+            data[1].append(record['asin'])
+            data[2].append(record['overall'])
     return data
 
 
 data = readData()
 dict = {}
-for key in data:
+for key in data[2]:
     dict[key] = dict.get(key, 0) + 1
 key_value = list(dict.keys())
 value_list = list(dict.values())
@@ -25,3 +27,24 @@ plt.ylabel("Number of Ratings")
 plt.savefig("RatingScoreDistribution.png")
 plt.show()
 
+dict = {}
+for key in data[0]:
+    dict[key] = dict.get(key, 0) + 1
+value_list = sorted(list(dict.values()),reverse=True)
+plt.plot(range(len(value_list)), value_list)
+plt.title("Users' Rating Distribution")
+plt.xlabel("Users")
+plt.ylabel("Number of Ratings")
+plt.savefig("UsersRatingDistribution.png")
+plt.show()
+
+dict = {}
+for key in data[1]:
+    dict[key] = dict.get(key, 0) + 1
+value_list = sorted(list(dict.values()),reverse=True)
+plt.plot(range(len(value_list)), value_list)
+plt.title("Items' Rating Distribution")
+plt.xlabel("Items")
+plt.ylabel("Number of Ratings")
+plt.savefig("ItemsRatingDistribution.png")
+plt.show()
